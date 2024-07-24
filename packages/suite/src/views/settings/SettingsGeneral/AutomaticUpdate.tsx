7db62dc96651fe1490be551a66ef77f5485a4c25
Peter Sanderson <dierk.deidel@protonmail.com>
2024-07-24 13:01:14 +0200

    chore(suite): deprecate useActions and remove its usage from Updater

--- packages/suite/src/views/settings/SettingsGeneral/AutomaticUpdate.tsx
+++ packages/suite/src/views/settings/SettingsGeneral/AutomaticUpdate.tsx
@@ -31,7 +31,7 @@ export const AutomaticUpdate = () => {
     };
 
     return (
-        <SettingsSectionItem anchorId={SettingsAnchor.Analytics}>
+        <SettingsSectionItem anchorId={SettingsAnchor.AutomaticUpdate}>
             <TextColumn
                 title={<Translation id="TR_ALLOW_AUTOMATIC_SUITE_UPDATES" />}
                 description={<Translation id="TR_ALLOW_AUTOMATIC_SUITE_UPDATES_DESCRIPTION" />}
